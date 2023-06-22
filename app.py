c7a747afebcd48dec16d1c1adc2721473085a1e5
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-21 21:58:51 -0600

    Getting AI description of input image, working

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+from dotenv import find_dotenv, load_dotenv
+from transformers import pipeline
+
 # img2text
+def img2text(url):
+    imageToText = pipeline('image-to-text', model="Salesforce/blip-image-captioning-base")
 
+    text = imageToText(url)[0]["generated_text"]
+    print(text) # a man and woman dancing on a dance floor
 
+    return text
 
 # LLM
 
 
 
 # text to speech
+
+
+
+load_dotenv(find_dotenv())
+
+img2text("photo_01.png")
+
