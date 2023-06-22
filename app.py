5b82ed91e407ab903a8fc1a31fafb0e3e9a1b050
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-21 22:02:21 -0600

    Sending context to LLM openAi

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline
+from langchain import PromptTemplate, LLMChain, OpenAI
 
 # img2text
 def img2text(url):
@@ -11,7 +12,25 @@ def img2text(url):
     return text
 
 # LLM
+def generate_story(scenario):
+    template = """
+    You are a story teller;
+    You can generate a short story based on a simple narrative, the story should be no more than 20 words;
 
+    CONTEXT: {scenario}
+    STORY:
+    """
+
+    prompt = PromptTemplate(template=template, input_variables=["scenario"])
+
+    story_llm = LLMChain(llm=OpenAI(
+        model_name="gpt-3.5-turbo", temperature=1), prompt=prompt, verbose=True
+    )
+
+    story = story_llm.predict(scenario=scenario)
+    print(story)
+
+    return story
 
 
 # text to speech
@@ -20,5 +39,5 @@ def img2text(url):
 
 load_dotenv(find_dotenv())
 
-img2text("photo_01.png")
+# img2text("photo_01.png")
 
